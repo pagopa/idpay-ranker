6431c78d340b85223736ab616c8ba59730a28b27
GiovanaSolorzano <giovanaelvira.solorzanochirieleison@emeal.nttdata.com>
2026-07-28 12:06:56 +0200

    update dto

--- src/main/java/it/gov/pagopa/ranker/domain/dto/VerifyDTO.java
+++ src/main/java/it/gov/pagopa/ranker/domain/dto/VerifyDTO.java
@@ -1,14 +1,8 @@
 package it.gov.pagopa.ranker.domain.dto;
 
-import com.azure.resourcemanager.monitor.models.OnboardingStatus;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -20,7 +14,7 @@ public class VerifyDTO {
     private String thersoldCode;
     private Long beneficiaryBudgetCentsMin;
     private Long beneficiaryBudgetCentsMax;
-    private boolean resultVerify;
+    private boolean blockingVerify;
 
 
 }
